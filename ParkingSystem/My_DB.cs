f8d71d98b8c03354fd2d51566fb06a8b2ae68b64
luuvancui <cuiluu@realshare.vn>
2020-12-08 21:05:35 +0700

    connect to databse

---
+++ ParkingSystem/My_DB.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSystem
+{
+    class My_DB
+    {
+        SqlConnection con = new SqlConnection();
+        public My_DB(string serverName, string username, string password)
+        {
+            string cs = "Data Source=" + serverName + ";Initial Catalog=PARKING;User ID=" + username + ";Password=" + password;
+            con.ConnectionString = cs;
+        }
+
+        public static bool isConnectExist(string serverName, string username, string password)
+        {
+            try
+            {
+                string cs = "Data Source=" + serverName + ";Initial Catalog=PARKING;User ID=" + username + ";Password=" + password;
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    conn.Open();
+                }
+            } catch(Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public SqlConnection getConnection
+        {
+            get
+            {
+                return con;
+            }
+        }
+
+        public void openConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
--- ParkingSystem/common/LoginForm.Designer.cs
+++ ParkingSystem/common/LoginForm.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBox_username = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBoxServerName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -47,58 +49,74 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Parking System";
             // 
-            // button1
+            // buttonLogin
             // 
-            this.button1.Location = new System.Drawing.Point(208, 375);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 46);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonLogin.Location = new System.Drawing.Point(208, 375);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(154, 46);
+            this.buttonLogin.TabIndex = 1;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
             // 
-            // textBox1
+            // textBox_username
             // 
-            this.textBox1.Location = new System.Drawing.Point(208, 228);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(304, 26);
-            this.textBox1.TabIndex = 3;
+            this.textBox_username.Location = new System.Drawing.Point(208, 274);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(304, 26);
+            this.textBox_username.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(72, 228);
+            this.label2.Location = new System.Drawing.Point(72, 274);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "Username:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(77, 304);
+            this.label3.Location = new System.Drawing.Point(77, 330);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(82, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Password:";
             // 
-            // textBox2
+            // textBox_password
             // 
-            this.textBox2.Location = new System.Drawing.Point(208, 304);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(304, 26);
-            this.textBox2.TabIndex = 3;
+            this.textBox_password.Location = new System.Drawing.Point(208, 330);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '*';
+            this.textBox_password.Size = new System.Drawing.Size(304, 26);
+            this.textBox_password.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(200, 131);
+            this.label4.Location = new System.Drawing.Point(200, 116);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(123, 46);
             this.label4.TabIndex = 4;
             this.label4.Text = "Login";
-            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxServerName
+            // 
+            this.textBoxServerName.Location = new System.Drawing.Point(208, 215);
+            this.textBoxServerName.Name = "textBoxServerName";
+            this.textBoxServerName.Size = new System.Drawing.Size(304, 26);
+            this.textBoxServerName.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Server Name:";
             // 
             // LoginForm
             // 
@@ -107,10 +125,12 @@
             this.ClientSize = new System.Drawing.Size(618, 450);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBoxServerName);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.buttonLogin);
             this.Controls.Add(this.label1);
             this.Name = "LoginForm";
             this.Text = "Login";
@@ -122,12 +142,14 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBox_username;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_password;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxServerName;
+        private System.Windows.Forms.Label label5;
     }
 }
 
